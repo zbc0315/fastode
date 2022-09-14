66a087ee7431e6ab44d156f452a48f3436c9ca00
zhang <zhangbc0315@outlook.com>
2022-09-14 15:17:43 +0800

    feat: add setup

---
+++ setup.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python 
+# -*- coding: utf-8 -*-
+# @Time    : 2022/7/4 14:56
+# @Author  : zbc@mail.ustc.edu.cn
+# @File    : setup.py
+# @Software: PyCharm
+
+from setuptools import setup
+
+with open('README.md', 'r', encoding='utf-8')as f:
+    long_description = f.read()
+
+setup(
+    name='fastode',
+    version='0.0.1',
+    author='zhang',
+    author_email='zhangbc0315@outlook.com',
+    url='',
+    description=u'A toolset to help make python programming faster',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    packages=['fastode'],
+    install_requires=[],
+    include_package_data=True,
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.5'
+)
+
+
+if __name__ == "__main__":
+    pass
